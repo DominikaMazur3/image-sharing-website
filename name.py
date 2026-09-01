deb12ec39bc80ade01a5d72e7d52e0b0b934561e
Dominika Mazur <92177055+DominikaMazur3@users.noreply.github.com>
2026-09-01 09:20:31 +0200

    Secure filename
    - secure filename
    - deleted unnecessary Message model
    - small template improvements

--- name.py
+++ name.py
@@ -1,21 +1,17 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_database.db'
 db = SQLAlchemy(app)
 db.app = app
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    content = db.Column(db.String(1000))
-    def __repr__(self):
-        return 'Message ' + self.content
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(256))
+    date_uploaded = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     def __repr__(self):
         return 'Image ' + self.filename
 with app.app_context():
@@ -25,8 +21,9 @@ with app.app_context():
 def placeholder():
     if request.method == 'POST':
         uploaded = request.files['file']
-        uploaded.save('static/uploaded/' + uploaded.filename)
-        db.session.add(Image(filename='static/uploaded/' + uploaded.filename))
+        new_filename = secure_filename(uploaded.filename)
+        uploaded.save('static/uploaded/' + new_filename)
+        db.session.add(Image(filename='static/uploaded/' + new_filename))
         db.session.commit()
         return redirect(url_for('placeholder'))
     return render_template('home.html', images=Image.query.all(),messages=Message.query.all())
